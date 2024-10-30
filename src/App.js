0886e9f15da9d6f35142d2fc495988b43a3f57a3
Shy Manuel <Shy Manuel>
2024-10-30 12:19:55 +0200

    added whatsapp btn component

--- src/App.js
+++ src/App.js
@@ -4,18 +4,19 @@ import Home from "./routes/home";
 import About from "./routes/about"; 
 import Service from "./routes/service"; 
 import Contact from "./routes/contact"; 
+import WhatsAppButton from "./components/WhatsAppButton"; 
 
 export default function App() { 
   return ( 
     <Router> 
       <div className="App"> 
-
         <Routes> 
           <Route path="/" element={<Home />} /> 
           <Route path="/about" element={<About />} /> 
           <Route path="/service" element={<Service />} /> 
           <Route path="/contact" element={<Contact />} /> 
         </Routes> 
+        <WhatsAppButton />
       </div> 
     </Router> 
   ); 
